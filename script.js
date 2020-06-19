27d745c9d082b056391b265c37c2c4153e80f48f
Berat <berat.aras@hotmail.com>
2020-06-20 01:14:02 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,102 @@
+const form = document.querySelector('form');
+const input = document.querySelector("#txtTaskName");
+const DeleteAll = document.querySelector("#btnDeleteAll");
+const TaskList = document.querySelector("#task-list");
+
+let items;
+
+//load items
+loadItems();
+
+//call listener
+eventListeners();
+
+function eventListeners(){
+    //submit event
+    form.addEventListener('submit', addNewItem);
+
+    //delete an item
+    TaskList.addEventListener('click', deleteItem);
+
+    //delete all items
+    DeleteAll.addEventListener('click', deleteAllItem);
+}
+
+function loadItems(){
+    items = getItemsFromLS();
+    items.forEach(function(item){
+        createItem(item)
+    })
+}
+
+//get items from Local Storage
+function getItemsFromLS(){
+    if(localStorage.getItem('items') === null){
+        items = [];
+    }else{
+        items = JSON.parse(localStorage.getItem('items'));
+    }
+    return items;
+}
+
+//set item to Local Storage
+function setItemToLS(text){
+    items = getItemsFromLS();
+    items.push(text);
+    localStorage.setItem('items', JSON.stringify(items))
+}
+
+function createItem(text){
+    //create li
+    const li = document.createElement('li');
+    li.className='list-group-item list-group-item-secondary';
+    li.appendChild(document.createTextNode(text));
+
+    //create a
+    const a = document.createElement('a');
+    a.classList = 'delete-item float-right';
+    a.setAttribute('href', '#');
+    a.innerHTML = '<i class="fas fa-times"></i>';
+
+    //add a to li
+    li.appendChild(a);
+
+    //add li to ul
+    TaskList.appendChild(li);
+}
+
+//add new item
+function addNewItem(e){
+    if(input.value === ''){
+        alert('Boş değer giremezsiniz.');
+    }else{
+
+        //create item
+        createItem(input.value);
+
+        //save to LS
+        setItemToLS(input.value);
+
+        //clear input
+        input.value = '';
+
+        e.preventDefault();
+    }
+}
+
+//delete item
+function deleteItem(e){
+    if(e.target.className === 'fas fa-times'){
+        if(confirm('are you sure?')){
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+    e.preventDefault();
+}
+
+//delete all item
+function deleteAllItem(e){
+    TaskList.innerHTML = '';
+
+    e.preventDefault();
+}
